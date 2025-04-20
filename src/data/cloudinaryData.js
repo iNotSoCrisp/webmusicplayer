456e5770a4de5517414a5b3cd53ab70cca6d513f
Shubh Arya <shubh4arya@gmail.com>
2025-04-21 01:33:52 +0530

    more songs added

--- src/data/cloudinaryData.js
+++ src/data/cloudinaryData.js
@@ -1,46 +1,93 @@
-// These songs will need to be uploaded to Cloudinary by you
-// The 'audioUrl' should be the direct URL to the audio file on Cloudinary
 export const streamableSongs = [
   {
     id: "cloud1",
     title: "4AM in Karachi",
     artist: "Talha Anjum",
     cover: "https://res.cloudinary.com/dkyknciiv/image/upload/v1744750976/4am_in_karachi_qmvtzs.jpg",
-    audioUrl: "https://res.cloudinary.com/dkyknciiv/video/upload/v1744750906/4AM_in_Karachi_-_PaagalWorld.Com.Se_yncqze.mp3" // Replace with your actual audio URL
+    audioUrl: "https://res.cloudinary.com/dkyknciiv/video/upload/v1744750906/4AM_in_Karachi_-_PaagalWorld.Com.Se_yncqze.mp3"
   },
   {
     id: "cloud2",
     title: "In the End",
     artist: "Linkin Park",
     cover: "https://res.cloudinary.com/dkyknciiv/image/upload/v1745064544/juky0mliph6rly0bkfo5.jpg",
-    audioUrl: "https://res.cloudinary.com/dkyknciiv/video/upload/v1745064260/In_The_End_Official_HD_Music_Video_-_Linkin_Park_oyf6r3.mp3" // Replace with your actual audio URL
+    audioUrl: "https://res.cloudinary.com/dkyknciiv/video/upload/v1745064260/In_The_End_Official_HD_Music_Video_-_Linkin_Park_oyf6r3.mp3"
   },
   {
     id: "cloud3",
     title: "Perfect",
     artist: "Ed Sheeran",
     cover: "https://res.cloudinary.com/dkyknciiv/image/upload/v1745064765/ycbch3kd4pxb6jyrqc0y.jpg",
-    audioUrl: "https://res.cloudinary.com/dkyknciiv/video/upload/v1745064257/Ed_Sheeran_-_Perfect_Official_Lyric_Video_wzabo4.mp3" // Replace with your actual audio URL
+    audioUrl: "https://res.cloudinary.com/dkyknciiv/video/upload/v1745064257/Ed_Sheeran_-_Perfect_Official_Lyric_Video_wzabo4.mp3"
   },
   {
     id: "cloud4",
     title: "Kaaghaz Ka Ghar",
     artist: "Jani",
     cover: "https://res.cloudinary.com/dkyknciiv/image/upload/v1745065427/hqdefault_dtkgoh.jpg",
-    audioUrl: "https://res.cloudinary.com/dkyknciiv/video/upload/v1745065378/JANI_-_Kaaghaz_Ka_Ghar_da2jxu.mp3" // Replace with your actual audio URL
+    audioUrl: "https://res.cloudinary.com/dkyknciiv/video/upload/v1745065378/JANI_-_Kaaghaz_Ka_Ghar_da2jxu.mp3"
   },
   {
     id: "cloud5",
     title: "Tum Tak",
     artist: "A.R. Rahman",
     cover: "https://res.cloudinary.com/dkyknciiv/image/upload/v1745065169/cz0avrqwzrjsqdsczzx4.jpg",
-    audioUrl: "https://res.cloudinary.com/dkyknciiv/video/upload/v1745065034/A.R._Rahman_-_Tum_Tak_Lyric_Video_Raanjhanaa_A._R._Rahman_Dhanush_Sonam_Kapoor_Javed_Ali_amakba.mp3" // Replace with your actual audio URL
+    audioUrl: "https://res.cloudinary.com/dkyknciiv/video/upload/v1745065034/A.R._Rahman_-_Tum_Tak_Lyric_Video_Raanjhanaa_A._R._Rahman_Dhanush_Sonam_Kapoor_Javed_Ali_amakba.mp3"
   },
   {
     id: "cloud6",
     title: "Memories",
     artist: "Maroon 5",
     cover: "https://res.cloudinary.com/dkyknciiv/image/upload/v1745065144/cz561tmjgieyarer12s0.jpg",
-    audioUrl: "https://res.cloudinary.com/dkyknciiv/video/upload/v1745064260/Maroon_5_-_Memories_Official_Video_a2f7k1.mp3" // Replace with your actual audio URL
+    audioUrl: "https://res.cloudinary.com/dkyknciiv/video/upload/v1745064260/Maroon_5_-_Memories_Official_Video_a2f7k1.mp3"
+  },
+  {
+    id: "cloud7",
+    title: "Snow (Hey Oh)",
+    artist: "Red Hot Chilli Peppers",
+    cover: "https://res.cloudinary.com/dkyknciiv/image/upload/v1745178200/ilyl4ounlzvewy4kxgrn.jpg",
+    audioUrl: "https://res.cloudinary.com/dkyknciiv/video/upload/v1745177515/Red_Hot_Chili_Peppers_-_Snow_Hey_Oh_Official_Music_Video_wbyeak.mp3"
+  },
+  {
+    id: "cloud8",
+    title: "Mockingbird",
+    artist: "Eminem",
+    cover: "https://res.cloudinary.com/dkyknciiv/image/upload/v1745177565/u6w5gwoduwt2isajv7x1.jpg",
+    audioUrl: "https://res.cloudinary.com/dkyknciiv/video/upload/v1745177524/Eminem_-_Mockingbird_Official_Music_Video_lxrmct.mp3"
+  },
+  {
+    id: "cloud9",
+    title: "Shaayar",
+    artist: "Bharat Chauhan & Seedhe Maut",
+    cover: "https://res.cloudinary.com/dkyknciiv/image/upload/v1745177772/teinramadnoge4sip3al.jpg",
+    audioUrl: "https://res.cloudinary.com/dkyknciiv/video/upload/v1745177512/Bharat_Chauhan_-_Shaayar_Official_Audio_ft._Seedhe_Maut_zrrwrc.mp3"
+  },
+  {
+    id: "cloud10",
+    title: "Kabira",
+    artist: "Pritam",
+    cover: "https://res.cloudinary.com/dkyknciiv/image/upload/v1745178149/artworks-000051031006-p3q9qo-t500x500_vvwoan.jpg",
+    audioUrl: "https://res.cloudinary.com/dkyknciiv/video/upload/v1745177505/Kabira_Full_Song_Yeh_Jawaani_Hai_Deewani_Pritam_Ranbir_Kapoor_Deepika_Padukone_d2w8gp.mp3"
+  },
+  {
+    id: "cloud11",
+    title: "Old Town Road",
+    artist: "Lil Nas X",
+    cover: "https://res.cloudinary.com/dkyknciiv/image/upload/v1745177740/y92xirusqklipbysg80v.jpg",
+    audioUrl: "https://res.cloudinary.com/dkyknciiv/video/upload/v1745177503/Lil_Nas_X_-_Old_Town_Road_Official_Video_ft._Billy_Ray_Cyrus_dzpbzz.mp3"
+  },
+  {
+    id: "cloud12",
+    title: "Farq Hai",
+    artist: "Suzonn",
+    cover: "https://res.cloudinary.com/dkyknciiv/image/upload/v1745178175/z9xtgkh5uyv1qdg1obi7.jpg",
+    audioUrl: "https://res.cloudinary.com/dkyknciiv/video/upload/v1745177508/Farq_Hai_-_Suzonn_Official_Music_Video_e8kb1x.mp3"
+  },
+  {
+    id: "cloud13",
+    title: "Maand",
+    artist: "Bayaan",
+    cover: "https://res.cloudinary.com/dkyknciiv/image/upload/v1745177664/lro79wm1urkkpvivikcr.jpg",
+    audioUrl: "https://res.cloudinary.com/dkyknciiv/video/upload/v1745177502/Bayaan_Hasan_Raheem_Rovalio_-_Maand_Lyric_Video_cih00s.mp3"
   }
 ];
